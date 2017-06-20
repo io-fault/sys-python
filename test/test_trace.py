8e772b9bf8e506664a9f0a263aff34ceaf8d4485
fault <critical@fault.io>
2017-06-20 15:24:10 -0700

    Relocate tracing from fault.development.

---
+++ test/test_trace.py
@@ -0,0 +1,82 @@
+import itertools
+import collections
+from .. import tracing as module
+
+class RandomClass(object):
+	def rc_method(self):
+		i = 7
+		k = i + 2
+		return k
+
+	def outer_method(self):
+		k = 1
+		k += 2
+		self.rc_method()
+
+	@classmethod
+	def generate(Class):
+		i = Class()
+		i.rc_method()
+		i.outer_method()
+		i.rc_method()
+
+def traced_call(C):
+	C.subscribe()
+
+	rc = RandomClass()
+	rc.rc_method()
+	rc.outer_method()
+	rc.rc_method()
+
+	C.cancel()
+
+def test_Collector(test):
+	"""
+	# Exercise basic functionality by simulating events.
+	"""
+
+	cd = list()
+	ch = itertools.count()
+	CC = module.instr.Collector(cd.append, ch.__next__)
+
+	pd = list()
+	ph = itertools.count()
+	PC = module.Collector(pd.append, ph.__next__)
+
+	traced_call(CC); traced_call(PC)
+
+	test/pd != []
+	test/cd != []
+
+events = [
+	(('test/test_trace.py', 22, 30, 'test_collection'), 2, 0),
+	(('test/test_trace.py', 22, 31, 'test_collection'), 2, 1),
+	(('test/test_trace.py', 8, 8, 'rc_method'), 0, 2),
+	(('test/test_trace.py', 8, 9, 'rc_method'), 2, 3),
+	(('test/test_trace.py', 8, 10, 'rc_method'), 2, 4),
+	(('test/test_trace.py', 8, 11, 'rc_method'), 2, 5),
+	(('test/test_trace.py', 8, 11, 'rc_method'), 3, 6),
+	(('test/test_trace.py', 22, 32, 'test_collection'), 2, 7),
+	(('test/test_trace.py', 13, 13, 'outer_method'), 0, 8),
+	(('test/test_trace.py', 13, 14, 'outer_method'), 2, 9),
+	(('test/test_trace.py', 13, 15, 'outer_method'), 2, 10),
+	(('test/test_trace.py', 13, 16, 'outer_method'), 2, 11),
+	(('test/test_trace.py', 8, 8, 'rc_method'), 0, 12),
+	(('test/test_trace.py', 8, 9, 'rc_method'), 2, 13),
+	(('test/test_trace.py', 8, 10, 'rc_method'), 2, 14),
+	(('test/test_trace.py', 8, 11, 'rc_method'), 2, 15),
+	(('test/test_trace.py', 8, 11, 'rc_method'), 3, 16),
+	(('test/test_trace.py', 13, 16, 'outer_method'), 3, 17),
+	(('test/test_trace.py', 22, 34, 'test_collection'), 2, 18,),
+
+	(('test/test_trace.py', 8, 8, 'rc_method'), 0, 19),
+	(('test/test_trace.py', 8, 9, 'rc_method'), 2, 20),
+	(('test/test_trace.py', 8, 10, 'rc_method'), 2, 21),
+	(('test/test_trace.py', 8, 11, 'rc_method'), 2, 22),
+	(('test/test_trace.py', 8, 11, 'rc_method'), 3, 23),
+	(('test/test_trace.py', 22, 32, 'test_collection'), 2, 24),
+]
+
+if __name__ == '__main__':
+	from .. import libtest; import sys
+	libtest.execute(sys.modules[__name__])
---
+++ trace.py
@@ -0,0 +1,252 @@
+"""
+# Coverage and profiling data collection.
+
+# ! WARNING:
+	# &Collector instances *must* be per-thread in order for &measure to
+	# properly calculate call timings.
+
+# Common usage:
+
+#!/pl/python
+	collector, events = tracing.prepare()
+	with collector:
+		...
+	aggregate = tracing.measure(events)
+
+# [ Development Tasks ]
+
+	# - Refactor &measure as a stateful method that can be called during collection
+		# in order to reduce memory consumption during long runs.
+	# - Make use of interjection in order to perform collection maintenance
+		# for &measure; cancel collection, run measure, serialize new data(?), resume.
+
+# [ Properties ]
+
+# /Measurements
+	# A tuple of measured trace data.
+"""
+import sys
+import collections
+import functools
+import typing
+
+try:
+	from . import instr # C based collector.
+except ImportError:
+	instr = None
+
+# Measure uses the integer form.
+event_integers = {
+	'call': 0,
+	'exception': 1,
+	'line': 2,
+	'return': 3,
+
+	'c_call': 4,
+	'c_exception': 5,
+	'c_return': 6,
+}
+
+class Collector(object):
+	"""
+	# Python collector. &instr.Collector is used when available.
+	"""
+
+	def __init__(self, endpoint, time_delta):
+		self.endpoint = endpoint
+		self.delta = time_delta
+		self._partial = functools.partial(self._collect, endpoint, self.delta)
+
+	# append and time_delta are provided in partial.
+	def _collect(self,
+			append, time_delta,
+			frame, event, arg,
+			event_map=None,
+			isinstance=isinstance,
+		):
+		global event_integers
+
+		co = frame.f_code
+
+		append((
+			(co.co_filename, co.co_firstlineno, frame.f_lineno, co.co_name),
+			event_integers[event], time_delta(),
+		))
+
+		# None return cancels the trace.
+		return self._partial
+
+	def __call__(self, *args):
+		# __call__ methods aren't particularly efficient, so we return self._partial
+		# in the future.
+		return self._partial(*args)
+
+	def subscribe(self):
+		"""
+		# Subscribe to all events.
+		"""
+		sys.settrace(self)
+
+	def profile(self):
+		"""
+		# Subscribe to profile data only.
+		"""
+		sys.setprofile(self)
+
+	def cancel(self):
+		"""
+		# Cancel the collection of data in the current thread.
+		"""
+		sys.settrace(None)
+		sys.setprofile(None)
+
+	def __enter__(self):
+		self.subscribe()
+
+	def __exit__(self, *args):
+		self.cancel()
+
+sequence = (
+	'total',
+	'count',
+	'minimum',
+	'maximum',
+	'median',
+	'average',
+	'distance',
+	'variance',
+	'modes',
+)
+
+def prepare(
+		Sequence=list,
+		Chronometer=None,
+		Collector=(Collector if instr is None else instr.Collector),
+	) -> typing.Tuple[Collector, typing.Sequence]:
+	"""
+	# Construct trace event collection using a &list instance
+	# as the destination. This is the primary entry point for this module and should
+	# be used to create the necessary per-thread &Collector instances.
+
+	# [ Effects ]
+
+	# /product
+		# Returns a pair containing the &Collector instance and the events instance
+		# of the configured &Sequence type.
+	"""
+
+	if Chronometer is None:
+		# import here to avoid import-time dependency.
+		from ..chronometry.kernel import Chronometer
+
+	events = Sequence()
+	chronometer = Chronometer()
+	collector = Collector(events.append, chronometer.__next__)
+
+	return collector, events
+
+Measurements = typing.Tuple[
+	typing.Mapping[typing.Tuple, typing.Sequence],
+	typing.Mapping[str, collections.Counter],
+]
+
+def measure(
+		events:typing.Sequence,
+
+		TRACE_LINE = instr.TRACE_LINE,
+
+		TRACE_CALL = instr.TRACE_CALL,
+		TRACE_RETURN = instr.TRACE_RETURN,
+		TRACE_EXCEPTION = instr.TRACE_EXCEPTION,
+
+		TRACE_C_CALL = instr.TRACE_C_CALL,
+		TRACE_C_RETURN = instr.TRACE_C_RETURN,
+		TRACE_C_EXCEPTION = instr.TRACE_C_EXCEPTION,
+
+		IntegerSequence=list,
+		deque=collections.deque,
+		defaultdict=collections.defaultdict,
+		Counter=collections.Counter,
+	) -> Measurements:
+	"""
+	# Measure line counts and call times from the collected trace data.
+
+	# Coverage events and profile events should be processed here.
+
+	# [ Parameters ]
+
+	# /events
+		# The sequence of events identified as the endpoint of a &Collector instance.
+		# Usually, a triple whose first key is the calling context, the second is
+		# the traced events, and the third is the time index.
+
+	# [ Effects ]
+
+	# /Product
+		# A pair consisting of the exact call times, cumulative and resident, and the line counts.
+
+		# Each item in the tuple is a mapping. The line counts is a two-level mapping
+		# keyed with the filename followed with the line number. The line number is a key
+		# to a &collections.Counter instance. The exact timings is a mapping whose keys
+		# are tuples whose contents are the calling context of the time measurements. The
+		# value of the mapping is a sequence of pairs describing the cumulative and resident
+		# times of the call context (key).
+	"""
+
+	call_state = deque((0,))
+	subcall_state = deque((0,))
+
+	counts = defaultdict(Counter)
+	times = defaultdict(list)
+
+	# Calculate timings and hit counts.
+	path = deque()
+	for x in events:
+		(filename, func_lineno, lineno, func_name), event, delta = x
+		call = (filename, func_lineno, func_name)
+
+		call_state[-1] += delta
+		subcall_state[-1] += delta
+
+		if event == TRACE_LINE:
+			counts[filename][lineno] += 1
+		elif event in {TRACE_CALL, TRACE_C_CALL}:
+			if path:
+				parent = path[-1]
+			else:
+				parent = None
+			path.append(call)
+
+			counts[filename][lineno] += 1
+
+			# push call state for timing measurements
+			call_state.append(0)
+			subcall_state.append(0)
+		elif event in {TRACE_RETURN, TRACE_C_RETURN}:
+			counts[filename][lineno] += 1
+
+			# pop call state, inherit total
+			sum = call_state.pop()
+			if not call_state:
+				call_state.append(0)
+
+			# subcall does not inherit
+			call_state[-1] += sum
+
+			# get our inner state; sometimes consistent with call_state
+			inner = subcall_state.pop()
+			if not subcall_state:
+				subcall_state.append(0)
+
+			# Record the cumulative and resident *parts*.
+			# They need to be summed/aggregated for reporting purposes.
+			times[(parent, call)].extend((sum, inner))
+
+			# Pop parent.
+			parent = None
+			if path:
+				path.pop()
+				if path:
+					parent = path[-1]
+
+	return times, counts
