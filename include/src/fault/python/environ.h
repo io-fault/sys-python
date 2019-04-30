62b248b77f4c51f35bcc2753ce5a84ccdb09a8f9
fault <critical@fault.io>
2019-04-29 17:34:35 -0700

    Add ForEachLong

--- include/src/fault/python/environ.h
+++ include/src/fault/python/environ.h
@@ -63,12 +63,14 @@ PyImport_ImportAdjacent(const char *modname, const char *attribute)
 }
 #define import_sibling PyImport_ImportAdjacent
 
-#define _PyLoop_Error(ITER) goto _PYERR_LABEL_##ITER
-#define _PyLoop_PassThrough(ITEM, OUTPUT, ...) ((*(OUTPUT) = ITEM) ? NULL : NULL)
 #define _PyLoop_NULL_INJECTION() ;
 #define PyLoop_ITEM _ITEM
+#define _PyLoop_Error(ITER) goto _PYERR_LABEL_##ITER
+#define _PyLoop_PassThrough(ITEM, OUTPUT, ...) ((*(OUTPUT) = ITEM) ? NULL : NULL)
+#define _PyLoop_ConvertLong(ITEM, OUTPUT, ...) ((*(OUTPUT) = PyLong_AsLong(ITEM)) == -1 ? -1 : 0)
+#define _PyLoop_ConvertLongFailure -1 && (PyErr_Occurred() != NULL)
 
-#define _PyLoop_Iterator(INJECTION, CONVERT_SUCCESS, CONVERT, GETITER, ITER, ...) \
+#define _PyLoop_Iterator(INJECTION, _CONVERT_SUCCESS, _CONVERT, GETITER, ITER, ...) \
 { \
 	PyObj _ITER = NULL; \
 	PyObj _PyLoop_ITEM = NULL; \
@@ -82,7 +84,7 @@ PyImport_ImportAdjacent(const char *modname, const char *attribute)
 	{ \
 		while ((_PyLoop_ITEM = PyIter_Next(_ITER)) != NULL) \
 		{ \
-			if (CONVERT(_PyLoop_ITEM, __VA_ARGS__) != CONVERT_SUCCESS) \
+			if (_CONVERT(_PyLoop_ITEM, __VA_ARGS__) != _CONVERT_SUCCESS) \
 			{ \
 				Py_XDECREF(_PyLoop_ITEM); \
 				_PyLoop_ITEM = NULL; \
@@ -111,23 +113,24 @@ PyImport_ImportAdjacent(const char *modname, const char *attribute)
 	} \
 }
 
-/*
- * PyLoop_ForEach(iter, &obj)
- * {
- *     ...
- * }
- * PyLoop_CatchError(iter)
- * {
- *     ...
- * }
- * PyLoop_End(iter)
- */
-
 #define PyLoop_ForEachTuple(ITER, ...) \
 	_PyLoop_Iterator(_PyLoop_NULL_INJECTION, 1, PyArg_ParseTuple, PyObject_GetIter, ITER, __VA_ARGS__)
 #define PyLoop_ForEachDictItem(DICT, ...) \
 	_PyLoop_Iterator(_PyLoop_NULL_INJECTION, 1, PyArg_ParseTuple, _PyLoop_DictionaryItems, DICT, __VA_ARGS__)
+#define PyLoop_ForEachLong(ITER, ...) \
+	_PyLoop_Iterator(_PyLoop_NULL_INJECTION, _PyLoop_ConvertLongFailure, _PyLoop_ConvertLong, PyObject_GetIter, ITER, __VA_ARGS__)
 
-/* For Each item in iterator loop with no conversion. */
+/*
+	// For Each item in iterator loop with no conversion.
+	PyLoop_ForEach(iter, &obj)
+	{
+	    ...
+	}
+	PyLoop_CatchError(iter)
+	{
+	    ...
+	}
+	PyLoop_End(iter)
+*/
 #define PyLoop_ForEach(ITER, ...) \
 	_PyLoop_Iterator(_PyLoop_NULL_INJECTION, NULL, _PyLoop_PassThrough, PyObject_GetIter, ITER, __VA_ARGS__)
